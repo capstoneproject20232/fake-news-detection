981915668108011d2285f0c6847c7d2b97c87ddf
prabhjyot9 <prabhjyot.wadhwa@gmail.com>
2023-10-29 05:15:05 +0530

    SCRUM-22 <message>

--- hello.py
+++ hello.py
@@ -1,1 +1,7 @@
+<<<<<<< HEAD
+=======
+
+Remember when it wasn t acceptable to be an anti-American in America? It s great to see at least one woman who is standing up with a few others on the board against these anti-Americans who can t be bothered with the pledge. Watch
+
+>>>>>>> 090536a8d1c5b954c83c5b05724dca180e150804
 Remember when it wasn t acceptable to be an anti-American in America? It s great to see at least one woman who is standing up with a few others on the board against these anti-Americans who can t be bothered with the pledge. Watch
